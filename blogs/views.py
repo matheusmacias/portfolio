5bf4b7f2799a987695589d8d8aac867b743fbc30
matheusmacias <matheusmacias3@gmail.com>
2023-12-31 08:33:22 -0300

    refactor: Enhance the section of the completed work

--- blogs/views.py
+++ blogs/views.py
@@ -8,6 +8,9 @@ class ArticleDetailView(DetailView):
     template_name = 'details.html'
     context_object_name = 'article'
 
+    def get_queryset(self):
+        return Article.objects.all().exclude(tags__name__in=['Trabalhos Realizados']).order_by('-created_at')
+
 
 class ArticleListView(ListView):
     model = Article
@@ -16,3 +19,21 @@ class ArticleListView(ListView):
 
     def get_queryset(self):
         return Article.objects.all().exclude(tags__name__in=['Trabalhos Realizados']).order_by('-created_at')
+
+
+class ArticleWorkListView(ListView):
+    model = Article
+    template_name = 'jobs-list.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.all().filter(tags__name__in=['Trabalhos Realizados']).order_by('-created_at')
+
+
+class ArticleWorkDetailView(DetailView):
+    model = Article
+    template_name = 'details.html'
+    context_object_name = 'article'
+
+    def get_queryset(self):
+        return Article.objects.all().filter(tags__name__in=['Trabalhos Realizados']).order_by('-created_at')
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import Index, robots_txt
+from blogs.views import ArticleWorkListView, ArticleWorkDetailView
 
 app_name = 'main'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('robots.txt', robots_txt, name='robots_txt'),
+    path('trabalhos/', include([
+        path('', ArticleWorkListView.as_view(), name='article_work_list'),
+        path('<slug:slug>/', ArticleWorkDetailView.as_view(), name='article_work_detail'),
+    ])),
 ]
